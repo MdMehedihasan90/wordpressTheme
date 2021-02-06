1704ce2b5e570fad2215c359c9a2f97a3a07f366
MdMehedihasan90 <mehedi.peace90@gmail.com>
2021-02-06 23:36:45 +0600

    Second Update

--- wp-content/themes/mh/functions.php
+++ wp-content/themes/mh/functions.php
@@ -1,25 +1,33 @@
 <?php
+    // will add appearance-> Menu 
     add_theme_support('menus');
+    //will add appearance -> widgets 
     add_theme_support('widgets');
+    //will add appearance -> Header
     add_theme_support('custom-header');
+    //will add appearance -> Background; which will create theme customize option
     add_theme_support('custom-background');
+    //will add post -> add new; which will create featured image 
     add_theme_support('post-thumbnails');
+    //will add post -> add new; which will create Post Format
     add_theme_support('post-formats', ['audio','video']);
+    //will add customize -> header -> Logo; which will add the logo
     add_theme_support('custom-logo');
 
-    //Sidebar Registration
+    //widgets control panel
     register_sidebar([
         'name' => 'Custom Sidebar',
         'id'   => 'custom_sidebar',
         'description' => 'This is custom sidebar' 
 
     ]);
+    //widgets control panel
     register_sidebar([
         'name' => 'Register Sidebar',
         'id'   => 'register_sidebar',
         'description' => 'This is register sidebar'
     ]);
-
+    //create menus which will select as like as Primary menu, footer menu etc.
     register_nav_menus([
         'first_menu' => 'First Menu',
         'second_menu' => 'Second Menu'
